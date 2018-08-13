b58bc06b4bed88f3b7c9540b08ffb9e83302ce59
Ron Gierlach <rongierlach@gmail.com>
2018-08-13 13:14:02 -0700

    lint

--- test/methods/sum.js
+++ test/methods/sum.js
@@ -6,10 +6,10 @@ Test('sum', function () {
         Test.isTrue(expected.eq(value));
     }
 
-    const expectedSum = new BigNumber(600)
-    t(expectedSum, BigNumber.sum(100, 200, 300))
-    t(expectedSum, BigNumber.sum('100', '200', '300'))
-    t(expectedSum, BigNumber.sum(new BigNumber(100), new BigNumber(200), new BigNumber(300)))
-    t(expectedSum, BigNumber.sum(100, '200', new BigNumber(300)))
-    t(expectedSum, BigNumber.sum(99.9, 200.05, 300.05))
+    const expectedSum = new BigNumber(600);
+    t(expectedSum, BigNumber.sum(100, 200, 300));
+    t(expectedSum, BigNumber.sum('100', '200', '300'));
+    t(expectedSum, BigNumber.sum(new BigNumber(100), new BigNumber(200), new BigNumber(300)));
+    t(expectedSum, BigNumber.sum(100, '200', new BigNumber(300)));
+    t(expectedSum, BigNumber.sum(99.9, 200.05, 300.05));
 })
