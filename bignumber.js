2603bfd93e0e51a090b138561a79249f8480156e
Michael Mclaughlin <M8ch88l@gmail.com>
2022-12-04 12:42:45 +0000

    v9.1.1

--- bignumber.js
+++ bignumber.js
@@ -2,7 +2,7 @@
   'use strict';
 
 /*
- *      bignumber.js v9.1.0
+ *      bignumber.js v9.1.1
  *      A JavaScript library for arbitrary-precision arithmetic.
  *      https://github.com/MikeMcl/bignumber.js
  *      Copyright (c) 2022 Michael Mclaughlin <M8ch88l@gmail.com>
--- bignumber.mjs
+++ bignumber.mjs
@@ -1,5 +1,5 @@
 /*
- *      bignumber.js v9.1.0
+ *      bignumber.js v9.1.1
  *      A JavaScript library for arbitrary-precision arithmetic.
  *      https://github.com/MikeMcl/bignumber.js
  *      Copyright (c) 2022 Michael Mclaughlin <M8ch88l@gmail.com>
